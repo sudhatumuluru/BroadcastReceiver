a3a716576085f2d458a14249f022d97fbd7f5a1d
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-21 17:20:21 -0700

    first commit

---
+++ app/src/main/java/com/cmpe277/farmmanager/FarmManager.java
@@ -0,0 +1,13 @@
+package com.cmpe277.farmmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_manager);
+    }
+}
