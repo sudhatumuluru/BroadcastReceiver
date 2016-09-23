575414fe9d5c16d01f95a6600bf235183ed5f952
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-22 22:15:09 -0700

    completed and working

--- app/src/main/java/com/cmpe277/farmmanager/FarmManager.java
+++ app/src/main/java/com/cmpe277/farmmanager/FarmManager.java
@@ -1,13 +1,65 @@
 package com.cmpe277.farmmanager;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FarmManager extends AppCompatActivity {
+
+    public static int temperature;
+    private Button turnFanOn;
+    private Button turnFanSprinklerOn;
+    private String FAN_ON = "FAN_ON";
+    private String FAN_SPRINKLER_ON = "FAN_SPRINKLER_ON";
+    //private String STATUS;
+    //String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_manager);
+        turnFanOn = (Button) findViewById(R.id.TurnFanOn);
+        turnFanSprinklerOn = (Button) findViewById(R.id.turnFanSprinklerOn);
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(FarmManager.this, "Now the temperature is :" + temperature,
+                Toast.LENGTH_LONG).show();
+        if(temperature >  70 && temperature < 90){
+            turnFanSprinklerOn.setVisibility(View.GONE);
+            //turnFanOn.setEnabled(true);
+            //turnFanSprinklerOn.setEnabled(false);
+            //status = "FAN_ON";
+
+        }else if(temperature >90 && temperature <125){
+            turnFanOn.setVisibility(View.GONE);
+            //turnFanOn.setEnabled(false);
+            //turnFanSprinklerOn.setEnabled(true);
+            //status = "FAN_SPRINKLER_ON";
+        }
+        /* Intent intent = new Intent("poultry.farm.iot.status");
+        intent.putExtra(STATUS, status);
+        sendBroadcast(intent); */
+    }
+
+    public void onTurnFanOn(View v){
+        Intent intent = new Intent("poultry.farm.on.fan.sprinkler");
+        intent.putExtra("iotMode", FAN_ON);
+        sendBroadcast(intent);
+    }
+
+    public void onTurnFanSprinklerOn(View v){
+        Intent intent = new Intent("poultry.farm.on.fan.sprinkler");
+        intent.putExtra("iotMode", FAN_SPRINKLER_ON);
+        sendBroadcast(intent);
     }
 }
---
+++ app/src/main/java/com/cmpe277/farmmanager/GetValueBroadcastReceiver.java
@@ -0,0 +1,25 @@
+package com.cmpe277.farmmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class GetValueBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(intent.getAction().equals("poultry.farm.current.temperature")) {
+
+            //FarmManager.flag = true;
+            FarmManager.temperature = intent.getExtras().getInt("TEMPERATURE");
+
+            Intent Farmintent = new Intent(context,FarmManager.class);
+            Farmintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(Farmintent);
+
+        }
+
+    }
+}
